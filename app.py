77fa5c7d6469d2da4bd28e01e109067c4c11ef69
Santhosh B <Santhosh.B@efi.com>
2019-12-30 23:06:40 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-from flask import Flask,render_template,url_for,request
-import pandas as pd 
-
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
 import pickle
 
-# load the model from disk
-loaded_model=pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+
 app = Flask(__name__)
+model = pickle.load(open('random_forest_regression_model.pkl','rb'))
+
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+    #return 'Hello World'
+    return render_template('home.html')
+    #return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods = ['POST'])
 def predict():
-    df=pd.read_csv('real_2018.csv')
-    my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
-    my_prediction=my_prediction.tolist()
-    return render_template('result.html',prediction = my_prediction)
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    #output = round(prediction[0], 2)
+    return render_template('home.html', prediction_text= 'Delhi AQI is $ {}'.format(prediction[0]))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
 
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
